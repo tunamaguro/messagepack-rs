c9fe4eff1f971de3f0f064e159f5ad26e3c20323
tunamaguro <tsu7magu6@gmail.com>
2025-02-23 13:31:47 +0000

    test: rename bench

--- crates/messagepack-bench/benches/serialization.rs
+++ crates/messagepack-bench/benches/serialization.rs
@@ -8,135 +8,140 @@ fn main() {
     divan::main();
 }
 
+#[divan::bench_group]
 mod primtive_type {
     use messagepack_bench::PrimitiveTypes;
     use serde::Serialize;
 
-    #[divan::bench]
-    fn messagepack_serde(bencher: divan::Bencher) {
+    #[divan::bench(name = "messagepack_serde_primitive")]
+    fn messagepack_serde_(bencher: divan::Bencher) {
         let s = divan::black_box(rand::random::<PrimitiveTypes>());
 
         bencher.bench(move || {
             let buf = &mut [0_u8; 4096 * 10];
-            messagepack_serde::ser::to_slice(&s, buf).unwrap();
+            messagepack_serde::ser::to_slice(&s, divan::black_box(buf)).unwrap();
         });
     }
 
-    #[divan::bench]
-    fn rmp_serde(bencher: divan::Bencher) {
+    #[divan::bench(name = "rmp_serde_primitive")]
+    fn rmp_serde_(bencher: divan::Bencher) {
         let s = divan::black_box(rand::random::<PrimitiveTypes>());
 
         bencher.bench(move || {
             let mut buf = Vec::with_capacity(4096 * 10);
-            let mut ser = rmp_serde::Serializer::new(&mut buf);
+            let mut ser = rmp_serde::Serializer::new(divan::black_box(&mut buf));
 
             s.serialize(&mut ser).unwrap();
         });
     }
 }
 
+#[divan::bench_group]
 mod str_type {
     use messagepack_bench::StringTypes;
     use serde::Serialize;
 
-    #[divan::bench]
+    #[divan::bench(name = "messagepack_serde_str")]
     fn messagepack_serde(bencher: divan::Bencher) {
         let s = divan::black_box(StringTypes::default());
 
         bencher.bench(move || {
             let buf = &mut [0_u8; 4096 * 10];
-            messagepack_serde::ser::to_slice(&s, buf).unwrap();
+            messagepack_serde::ser::to_slice(&s, divan::black_box(buf)).unwrap();
         });
     }
 
-    #[divan::bench]
+    #[divan::bench(name = "rmp_serde_str")]
     fn rmp_serde(bencher: divan::Bencher) {
         let s = divan::black_box(StringTypes::default());
 
         bencher.bench(move || {
             let mut buf = Vec::with_capacity(4096 * 10);
-            let mut ser = rmp_serde::Serializer::new(&mut buf);
+            let mut ser = rmp_serde::Serializer::new(divan::black_box(&mut buf));
 
             s.serialize(&mut ser).unwrap();
         });
     }
 }
 
+#[divan::bench_group]
 mod array_type {
     use messagepack_bench::ArrayTypes;
     use serde::Serialize;
 
-    #[divan::bench]
+    #[divan::bench(name = "messagepack_serde_array")]
     fn messagepack_serde(bencher: divan::Bencher) {
         let s = divan::black_box(ArrayTypes::default());
 
         bencher.bench(move || {
             let buf = &mut [0_u8; 4096 * 10];
-            messagepack_serde::ser::to_slice(&s, buf).unwrap();
+            messagepack_serde::ser::to_slice(&s, divan::black_box(buf)).unwrap();
         });
     }
 
-    #[divan::bench]
+    #[divan::bench(name = "rmp_serde_array")]
     fn rmp_serde(bencher: divan::Bencher) {
         let s = divan::black_box(ArrayTypes::default());
 
         bencher.bench(move || {
             let mut buf = Vec::with_capacity(4096 * 10);
-            let mut ser = rmp_serde::Serializer::new(&mut buf);
+            let mut ser = rmp_serde::Serializer::new(divan::black_box(&mut buf));
 
             s.serialize(&mut ser).unwrap();
         });
     }
 }
 
+#[divan::bench_group]
 mod byte_type {
     use messagepack_bench::ByteType;
     use serde::Serialize;
 
-    #[divan::bench]
+    #[divan::bench(name = "messagepack_serde_byte")]
     fn messagepack_serde(bencher: divan::Bencher) {
         let s = divan::black_box(ByteType::default());
 
         bencher.bench(move || {
             let buf = &mut [0_u8; 4096 * 10];
-            messagepack_serde::ser::to_slice(&s, buf).unwrap();
+            messagepack_serde::ser::to_slice(&s, divan::black_box(buf)).unwrap();
         });
     }
 
-    #[divan::bench]
+    #[divan::bench(name = "rmp_serde_byte")]
     fn rmp_serde(bencher: divan::Bencher) {
         let s = divan::black_box(ByteType::default());
 
         bencher.bench(move || {
             let mut buf = Vec::with_capacity(4096 * 10);
-            let mut ser = rmp_serde::Serializer::new(&mut buf);
+            let mut ser = rmp_serde::Serializer::new(divan::black_box(&mut buf));
 
             s.serialize(&mut ser).unwrap();
         });
     }
 }
 
+#[divan::bench_group]
 mod composite_type {
     use messagepack_bench::CompositeType;
     use serde::Serialize;
 
-    #[divan::bench]
+    #[divan::bench(name = "messagepack_serde_composite")]
     fn messagepack_serde(bencher: divan::Bencher) {
         let s = divan::black_box(CompositeType::default());
 
         bencher.bench(move || {
             let buf = &mut [0_u8; 4096 * 10];
-            messagepack_serde::ser::to_slice(&s, buf).unwrap();
+            messagepack_serde::ser::to_slice(&s, divan::black_box(buf)).unwrap();
         });
     }
 
-    #[divan::bench]
+    #[divan::bench(name = "rmp_serde_composite")]
     fn rmp_serde(bencher: divan::Bencher) {
         let s = divan::black_box(CompositeType::default());
 
         bencher.bench(move || {
             let mut buf = Vec::with_capacity(4096 * 10);
-            let mut ser = rmp_serde::Serializer::new(&mut buf);
+            let mut ser = rmp_serde::Serializer::new(divan::black_box(&mut buf));
 
             s.serialize(&mut ser).unwrap();
         });
