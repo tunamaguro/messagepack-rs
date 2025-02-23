729f104bdfc4257cfc4f7117b801efaecba10b41
tunamaguro <tsu7magu6@gmail.com>
2025-02-23 06:40:10 +0000

    feat: impl data encoder

--- crates/messagepack-core/src/encode/map.rs
+++ crates/messagepack-core/src/encode/map.rs
@@ -1,80 +1,85 @@
-use core::{borrow::Borrow, marker::PhantomData, ops::Deref};
+use core::{borrow::Borrow, cell::RefCell, marker::PhantomData, ops::Deref};
 
 use super::{Encode, Error, Result};
 use crate::formats::Format;
 
-pub struct MapSliceEncoder<'data, B, K, V> {
-    data: &'data [B],
-    _phantom: PhantomData<(K, V)>,
+pub trait KVEncode {
+    fn encode<T>(&self, buf: &mut T) -> Result<usize>
+    where
+        T: Extend<u8>;
+    fn encode_to_iter_mut<'a>(&self, buf: &mut impl Iterator<Item = &'a mut u8>) -> Result<usize>;
 }
 
-impl<'data, B, K, V> MapSliceEncoder<'data, B, K, V> {
-    pub fn new(data: &'data [B]) -> Self {
-        Self {
-            data,
-            _phantom: Default::default(),
-        }
-    }
-}
-
-impl<'data, B, K, V> Deref for MapSliceEncoder<'data, B, K, V> {
-    type Target = &'data [B];
-    fn deref(&self) -> &Self::Target {
-        &self.data
-    }
-}
-
-impl<B, K, V> Encode for MapSliceEncoder<'_, B, K, V>
-where
-    B: Borrow<(K, V)>,
-    K: Encode,
-    V: Encode,
-{
+impl<KV: KVEncode> KVEncode for &KV {
     fn encode<T>(&self, buf: &mut T) -> Result<usize>
     where
         T: Extend<u8>,
     {
-        let self_len = self.data.len();
-        let format_len = match self_len {
+        KV::encode(&self, buf)
+    }
+
+    fn encode_to_iter_mut<'a>(&self, buf: &mut impl Iterator<Item = &'a mut u8>) -> Result<usize> {
+        KV::encode_to_iter_mut(&self, buf)
+    }
+}
+
+impl<K: Encode, V: Encode> KVEncode for (K, V) {
+    fn encode<T>(&self, buf: &mut T) -> Result<usize>
+    where
+        T: Extend<u8>,
+    {
+        let (k, v) = self;
+        let k_len = k.encode(buf)?;
+        let v_len = v.encode(buf)?;
+        Ok(k_len + v_len)
+    }
+    fn encode_to_iter_mut<'a>(&self, buf: &mut impl Iterator<Item = &'a mut u8>) -> Result<usize> {
+        let (k, v) = self;
+        let k_len = k.encode_to_iter_mut(buf)?;
+        let v_len = v.encode_to_iter_mut(buf)?;
+        Ok(k_len + v_len)
+    }
+}
+
+pub struct MapFormatEncoder(pub usize);
+impl MapFormatEncoder {
+    pub fn new(size: usize) -> Self {
+        Self(size)
+    }
+}
+
+impl Encode for MapFormatEncoder {
+    fn encode<T>(&self, buf: &mut T) -> Result<usize>
+    where
+        T: Extend<u8>,
+    {
+        match self.0 {
             0x00..=0xf => {
-                let cast = self_len as u8;
+                let cast = self.0 as u8;
                 let it = Format::FixMap(cast).into_iter();
                 buf.extend(it);
                 Ok(1)
             }
             0x10..=0xffff => {
-                let cast = self_len as u16;
+                let cast = self.0 as u16;
                 let it = Format::Map16.into_iter().chain(cast.to_be_bytes());
                 buf.extend(it);
                 Ok(3)
             }
             0x10000..=0xffffffff => {
-                let cast = self_len as u32;
+                let cast = self.0 as u32;
                 let it = Format::Map32.into_iter().chain(cast.to_be_bytes());
                 buf.extend(it);
                 Ok(5)
             }
             _ => Err(Error::InvalidFormat),
-        }?;
-
-        let map_len = self
-            .data
-            .iter()
-            .flat_map(|v| {
-                let (k, v) = v.borrow();
-                let k_len = k.encode(buf);
-                let v_len = v.encode(buf);
-                [k_len, v_len]
-            })
-            .try_fold(0, |acc, v| v.map(|n| acc + n))?;
-        Ok(format_len + map_len)
+        }
     }
     fn encode_to_iter_mut<'a>(&self, buf: &mut impl Iterator<Item = &'a mut u8>) -> Result<usize> {
-        let self_len = self.data.len();
-        let format_len = match self_len {
+        match self.0 {
             0x00..=0xf => {
                 const SIZE: usize = 1;
-                let cast = self_len as u8;
+                let cast = self.0 as u8;
                 let it = &mut Format::FixMap(cast).into_iter();
 
                 for (byte, to) in it.zip(buf.by_ref()) {
@@ -89,7 +94,7 @@ where
             }
             0x10..=0xffff => {
                 const SIZE: usize = 3;
-                let cast = self_len as u16;
+                let cast = self.0 as u16;
                 let it = &mut Format::Map16.into_iter().chain(cast.to_be_bytes());
 
                 for (byte, to) in it.zip(buf.by_ref()) {
@@ -104,7 +109,7 @@ where
             }
             0x10000..=0xffffffff => {
                 const SIZE: usize = 5;
-                let cast = self_len as u32;
+                let cast = self.0 as u32;
                 let it = &mut Format::Map32.into_iter().chain(cast.to_be_bytes());
                 for (byte, to) in it.zip(buf.by_ref()) {
                     *to = byte;
@@ -117,18 +122,96 @@ where
                 }
             }
             _ => Err(Error::InvalidFormat),
-        }?;
+        }
+    }
+}
 
+pub struct MapDataEncoder<I, J, KV> {
+    data: RefCell<J>,
+    _phantom: PhantomData<(I, J, KV)>,
+}
+
+impl<I, KV> MapDataEncoder<I, I::IntoIter, KV>
+where
+    I: IntoIterator<Item = KV>,
+{
+    pub fn new(data: I) -> Self {
+        Self {
+            data: RefCell::new(data.into_iter()),
+            _phantom: Default::default(),
+        }
+    }
+}
+
+impl<I, J, KV> Encode for MapDataEncoder<I, J, KV>
+where
+    J: Iterator<Item = KV>,
+    KV: KVEncode,
+{
+    fn encode<T>(&self, buf: &mut T) -> Result<usize>
+    where
+        T: Extend<u8>,
+    {
         let map_len = self
             .data
-            .iter()
-            .flat_map(|v| {
-                let (k, v) = v.borrow();
-                let k_len = k.encode_to_iter_mut(buf);
-                let v_len = v.encode_to_iter_mut(buf);
-                [k_len, v_len]
-            })
+            .borrow_mut()
+            .by_ref()
+            .map(|kv| kv.encode(buf))
             .try_fold(0, |acc, v| v.map(|n| acc + n))?;
+        Ok(map_len)
+    }
+
+    fn encode_to_iter_mut<'a>(&self, buf: &mut impl Iterator<Item = &'a mut u8>) -> Result<usize> {
+        let map_len = self
+            .data
+            .borrow_mut()
+            .by_ref()
+            .map(|kv| kv.encode_to_iter_mut(buf))
+            .try_fold(0, |acc, v| v.map(|n| acc + n))?;
+        Ok(map_len)
+    }
+}
+
+pub struct MapSliceEncoder<'data, KV> {
+    data: &'data [KV],
+    _phantom: PhantomData<KV>,
+}
+
+impl<'data, KV> MapSliceEncoder<'data, KV> {
+    pub fn new(data: &'data [KV]) -> Self {
+        Self {
+            data,
+            _phantom: Default::default(),
+        }
+    }
+}
+
+impl<'data, KV> Deref for MapSliceEncoder<'data, KV> {
+    type Target = &'data [KV];
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+}
+
+impl<KV> Encode for MapSliceEncoder<'_, KV>
+where
+    KV: KVEncode,
+{
+    fn encode<T>(&self, buf: &mut T) -> Result<usize>
+    where
+        T: Extend<u8>,
+    {
+        let self_len = self.data.len();
+        let format_len = MapFormatEncoder::new(self_len).encode(buf)?;
+        let map_len = MapDataEncoder::new(self.data.iter()).encode(buf)?;
+
+        Ok(format_len + map_len)
+    }
+    fn encode_to_iter_mut<'a>(&self, buf: &mut impl Iterator<Item = &'a mut u8>) -> Result<usize> {
+        let self_len = self.data.len();
+        let format_len = MapFormatEncoder::new(self_len).encode_to_iter_mut(buf)?;
+        let map_len = MapDataEncoder::new(self.data.iter()).encode_to_iter_mut(buf)?;
+
         Ok(format_len + map_len)
     }
 }
@@ -173,27 +256,7 @@ where
     {
         let clone_map = self.map.clone();
         let self_len = clone_map.len();
-        let format_len = match self_len {
-            0x00..=0xf => {
-                let cast = self_len as u8;
-                let it = Format::FixMap(cast).into_iter();
-                buf.extend(it);
-                Ok(1)
-            }
-            0x10..=0xffff => {
-                let cast = self_len as u16;
-                let it = Format::Map16.into_iter().chain(cast.to_be_bytes());
-                buf.extend(it);
-                Ok(3)
-            }
-            0x10000..=0xffffffff => {
-                let cast = self_len as u32;
-                let it = Format::Map32.into_iter().chain(cast.to_be_bytes());
-                buf.extend(it);
-                Ok(5)
-            }
-            _ => Err(Error::InvalidFormat),
-        }?;
+        let format_len = MapFormatEncoder::new(self_len).encode(buf)?;
 
         let map_len = clone_map
             .flat_map(|v| {
@@ -208,53 +271,7 @@ where
     fn encode_to_iter_mut<'a>(&self, buf: &mut impl Iterator<Item = &'a mut u8>) -> Result<usize> {
         let clone_map = self.map.clone();
         let self_len = clone_map.len();
-        let format_len = match self_len {
-            0x00..=0xf => {
-                const SIZE: usize = 1;
-                let cast = self_len as u8;
-                let it = &mut Format::FixMap(cast).into_iter();
-
-                for (byte, to) in it.zip(buf.by_ref()) {
-                    *to = byte
-                }
-
-                if it.next().is_none() {
-                    Ok(SIZE)
-                } else {
-                    Err(Error::BufferFull)
-                }
-            }
-            0x10..=0xffff => {
-                const SIZE: usize = 3;
-                let cast = self_len as u16;
-                let it = &mut Format::Map16.into_iter().chain(cast.to_be_bytes());
-
-                for (byte, to) in it.zip(buf.by_ref()) {
-                    *to = byte;
-                }
-
-                if it.next().is_none() {
-                    Ok(SIZE)
-                } else {
-                    Err(Error::BufferFull)
-                }
-            }
-            0x10000..=0xffffffff => {
-                const SIZE: usize = 5;
-                let cast = self_len as u32;
-                let it = &mut Format::Map32.into_iter().chain(cast.to_be_bytes());
-                for (byte, to) in it.zip(buf.by_ref()) {
-                    *to = byte;
-                }
-
-                if it.next().is_none() {
-                    Ok(SIZE)
-                } else {
-                    Err(Error::BufferFull)
-                }
-            }
-            _ => Err(Error::InvalidFormat),
-        }?;
+        let format_len = MapFormatEncoder::new(self_len).encode_to_iter_mut(buf)?;
 
         let map_len = clone_map
             .flat_map(|v| {
