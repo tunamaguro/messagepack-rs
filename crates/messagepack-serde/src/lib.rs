e49eb9a86df0416617beff485bf40c4f28f6ff06
tunamaguro <tsu7magu6@gmail.com>
2025-02-26 13:16:53 +0000

    chore: pub top

--- crates/messagepack-serde/src/lib.rs
+++ crates/messagepack-serde/src/lib.rs
@@ -7,3 +7,9 @@ pub mod ser;
 
 pub use de::{Deserializer, Error as DeserializationError, from_slice};
 pub use ser::{Error as SerializationError, Serializer, to_slice};
+
+#[cfg(feature = "std")]
+pub use de::from_reader;
+
+#[cfg(feature = "std")]
+pub use ser::to_writer;
