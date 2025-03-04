c30795fb85802d08db751d460ff5336e063ab2c1
tunamaguro <tsu7magu6@gmail.com>
2025-03-04 14:13:09 +0000

    refactor: remove unused

--- crates/messagepack-serde/src/ser/num.rs
+++ crates/messagepack-serde/src/ser/num.rs
@@ -1,11 +1,9 @@
 use messagepack_core::{
     Encode,
-    encode::{float::EncodeMinimizeFloat, int::EncodeMinimizeInt},
+    encode::{Error, float::EncodeMinimizeFloat, int::EncodeMinimizeInt},
     io::IoWrite,
 };
 use num_traits::{Float, ToPrimitive};
-
-use super::error::Error;
 
 /// Decide how numeric values are encoded.
 pub trait NumEncoder<W: IoWrite> {
@@ -60,51 +58,51 @@ pub struct Exact;
 
 impl<W: IoWrite> NumEncoder<W> for Exact {
     fn encode_i8(v: i8, writer: &mut W) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(v.encode(writer)?)
+        v.encode(writer)
     }
 
     fn encode_i16(v: i16, writer: &mut W) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(v.encode(writer)?)
+        v.encode(writer)
     }
 
     fn encode_i32(v: i32, writer: &mut W) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(v.encode(writer)?)
+        v.encode(writer)
     }
 
     fn encode_i64(v: i64, writer: &mut W) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(v.encode(writer)?)
+        v.encode(writer)
     }
 
     fn encode_i128(v: i128, writer: &mut W) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(v.encode(writer)?)
+        v.encode(writer)
     }
 
     fn encode_u8(v: u8, writer: &mut W) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(v.encode(writer)?)
+        v.encode(writer)
     }
 
     fn encode_u16(v: u16, writer: &mut W) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(v.encode(writer)?)
+        v.encode(writer)
     }
 
     fn encode_u32(v: u32, writer: &mut W) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(v.encode(writer)?)
+        v.encode(writer)
     }
 
     fn encode_u64(v: u64, writer: &mut W) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(v.encode(writer)?)
+        v.encode(writer)
     }
 
     fn encode_u128(v: u128, writer: &mut W) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(v.encode(writer)?)
+        v.encode(writer)
     }
 
     fn encode_f32(v: f32, writer: &mut W) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(v.encode(writer)?)
+        v.encode(writer)
     }
 
     fn encode_f64(v: f64, writer: &mut W) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(v.encode(writer)?)
+        v.encode(writer)
     }
 }
 
@@ -178,7 +176,7 @@ impl LosslessMinimize {
         v: T,
         writer: &mut W,
     ) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(EncodeMinimizeInt(v).encode(writer)?)
+        EncodeMinimizeInt(v).encode(writer)
     }
 
     fn encode_float<T: Into<EncodeMinimizeFloat>, W: IoWrite>(
@@ -310,7 +308,7 @@ impl AggressiveMinimize {
         v: T,
         writer: &mut W,
     ) -> Result<usize, Error<<W as IoWrite>::Error>> {
-        Ok(EncodeMinimizeInt(v).encode(writer)?)
+        EncodeMinimizeInt(v).encode(writer)
     }
 
     fn encode_float<T: Float + Into<EncodeMinimizeFloat>, W: IoWrite>(
