2953aafa9aeeffa597798abeb1cc52f9f31bdc5c
tunamaguro <contact@tunamaguro.dev>
2025-09-08 21:24:16 +0000

    feat: array decode

--- messagepack-core/src/decode/array.rs
+++ messagepack-core/src/decode/array.rs
@@ -33,25 +33,52 @@ where
             _ => return Err(Error::UnexpectedFormat),
         };
 
-        let mut has_err = None;
         let mut buf_ptr = buf;
-        let collector = core::iter::repeat_n((), len).map_while(|_| match V::decode(buf_ptr) {
-            Ok((v, b)) => {
-                buf_ptr = b;
-                Some(v)
-            }
-            Err(e) => {
-                has_err = Some(e);
-                None
-            }
-        });
-        let res = Array::from_iter(collector);
+        let out = (0..len)
+            .map(|_| {
+                let (v, next) = V::decode(buf_ptr)?;
+                buf_ptr = next;
+                Ok::<_, Error>(v)
+            })
+            .collect::<Result<Array>>()?;
+        Ok((out, buf_ptr))
+    }
+}
 
-        if let Some(e) = has_err {
-            Err(e)
-        } else {
-            Ok((res, buf_ptr))
+impl<'a, const N: usize, V> Decode<'a> for [V; N]
+where
+    V: Decode<'a>,
+{
+    type Value = [V::Value; N];
+    fn decode(buf: &'a [u8]) -> Result<(Self::Value, &'a [u8])> {
+        let (format, buf) = Format::decode(buf)?;
+        match format {
+            Format::FixArray(_) | Format::Array16 | Format::Array32 => {
+                Self::decode_with_format(format, buf)
+            }
+            _ => Err(Error::UnexpectedFormat),
         }
+    }
+    fn decode_with_format(format: Format, buf: &'a [u8]) -> Result<(Self::Value, &'a [u8])> {
+        let (len, buf) = match format {
+            Format::FixArray(len) => (len.into(), buf),
+            Format::Array16 => NbyteReader::<2>::read(buf)?,
+            Format::Array32 => NbyteReader::<4>::read(buf)?,
+            _ => return Err(Error::UnexpectedFormat),
+        };
+        if len != N {
+            return Err(Error::InvalidData);
+        };
+
+        let mut tmp: [Option<V::Value>; N] = core::array::from_fn(|_| None);
+        let mut buf_ptr = buf;
+        for i in 0..N {
+            let (val, next) = V::decode(buf_ptr)?;
+            tmp[i] = Some(val);
+            buf_ptr = next
+        }
+        let out = core::array::from_fn(|i| tmp[i].take().expect("initialized"));
+        Ok((out, buf_ptr))
     }
 }
 
