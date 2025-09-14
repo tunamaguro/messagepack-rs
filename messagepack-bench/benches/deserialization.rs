f4ed2fd3104ca87ce088ab20267edbd29de5763d
tunamaguro <contact@tunamaguro.dev>
2025-09-14 10:06:14 +0000

    Reapply "test: complex bench"
    This reverts commit fa29b3a855af118c189545407ddd3572eec8f42a.

--- messagepack-bench/benches/deserialization.rs
+++ messagepack-bench/benches/deserialization.rs
@@ -98,3 +98,82 @@ fn deserialize_borrowed_rmp_serde(#[allow(unused_mut)] mut bencher: divan::Bench
         rmp_serde::from_slice::<StrTypesBorrowed>(buf).unwrap()
     });
 }
+
+// Copy from https://github.com/3Hren/msgpack-rust/blob/09eade54a06e68273ad438a2b9bcbbc08ea6f4dc/rmpv-tests/benches/value.rs
+const COMPLEX: &[u8] = &[
+    0x94, 0x01, 0x00, 0x93, 0x91, 0x92, 0xa9, 0x31, 0x32, 0x37, 0x2e, 0x30, 0x2e, 0x30, 0x2e, 0x31,
+    0xcd, 0xe6, 0xc2, 0x01, 0x84, 0x00, 0x93, 0xa4, 0x72, 0x65, 0x61, 0x64, 0x80, 0x82, 0x00, 0x92,
+    0xa5, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x80, 0x01, 0x92, 0xa5, 0x65, 0x72, 0x72, 0x6f, 0x72, 0x80,
+    0x01, 0x93, 0xa5, 0x77, 0x72, 0x69, 0x74, 0x65, 0x80, 0x82, 0x00, 0x92, 0xa5, 0x76, 0x61, 0x6c,
+    0x75, 0x65, 0x80, 0x01, 0x92, 0xa5, 0x65, 0x72, 0x72, 0x6f, 0x72, 0x80, 0x02, 0x93, 0xa6, 0x72,
+    0x65, 0x6d, 0x6f, 0x76, 0x65, 0x80, 0x82, 0x00, 0x92, 0xa5, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x80,
+    0x01, 0x92, 0xa5, 0x65, 0x72, 0x72, 0x6f, 0x72, 0x80, 0x03, 0x93, 0xa4, 0x66, 0x69, 0x6e, 0x64,
+    0x80, 0x82, 0x00, 0x92, 0xa5, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x80, 0x01, 0x92, 0xa5, 0x65, 0x72,
+    0x72, 0x6f, 0x72, 0x80, 0x91, 0x93, 0x50, 0x51, 0x52,
+];
+
+#[divan::bench]
+fn deserialize_complex_messagepack_serde_from_slice(
+    #[allow(unused_mut)] mut bencher: divan::Bencher,
+) {
+    use messagepack_serde::{ValueRef, from_slice};
+
+    #[cfg(not(codspeed))]
+    {
+        bencher = bencher.counter(BytesCount::of_slice(&COMPLEX))
+    }
+
+    bencher.bench_local(|| {
+        let input = core::hint::black_box(COMPLEX);
+        let _val = from_slice::<ValueRef<'_>>(input).unwrap();
+    });
+}
+
+#[divan::bench]
+fn deserialize_complex_rmp_serde_from_slice(#[allow(unused_mut)] mut bencher: divan::Bencher) {
+    use rmp_serde::from_slice;
+    use rmpv::ValueRef;
+
+    #[cfg(not(codspeed))]
+    {
+        bencher = bencher.counter(BytesCount::of_slice(&COMPLEX))
+    }
+
+    bencher.bench_local(|| {
+        let input = core::hint::black_box(COMPLEX);
+        let _val: ValueRef<'_> = from_slice(input).unwrap();
+    });
+}
+
+#[divan::bench]
+fn deserialize_complex_messagepack_serde_from_reader(
+    #[allow(unused_mut)] mut bencher: divan::Bencher,
+) {
+    use messagepack_serde::{Value, from_reader};
+
+    #[cfg(not(codspeed))]
+    {
+        bencher = bencher.counter(BytesCount::of_slice(&COMPLEX))
+    }
+
+    bencher.bench_local(|| {
+        let input = core::hint::black_box(std::io::Cursor::new(COMPLEX));
+        let _val: Value = from_reader(input).unwrap();
+    });
+}
+
+#[divan::bench]
+fn deserialize_complex_rmp_serde_from_reader(#[allow(unused_mut)] mut bencher: divan::Bencher) {
+    use rmp_serde::from_read;
+    use rmpv::Value;
+
+    #[cfg(not(codspeed))]
+    {
+        bencher = bencher.counter(BytesCount::of_slice(&COMPLEX))
+    }
+
+    bencher.bench_local(|| {
+        let input = core::hint::black_box(std::io::Cursor::new(COMPLEX));
+        let _val: Value = from_read(input).unwrap();
+    });
+}
