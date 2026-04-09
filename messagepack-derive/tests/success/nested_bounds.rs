0e192a02f31aa233bf13c4e4f9b0e797ff8e15d0
tunamaguro <contact@tunamaguro.dev>
2026-04-09 09:21:58 +0000

    add test case

---
+++ messagepack-derive/tests/success/nested_bounds.rs
@@ -0,0 +1,22 @@
+use messagepack_core::decode::Decode;
+use messagepack_core::encode::Encode;
+use messagepack_core::io::SliceReader;
+use messagepack_derive::{Decode, Encode};
+
+#[derive(Debug, Encode, Decode)]
+struct One<T> {
+    value: T,
+    two: Box<Two<T>>,
+}
+
+#[derive(Debug, Encode, Decode)]
+struct Two<T> {
+    one: Box<One<T>>,
+}
+
+fn assert_derive<'de, T: Encode + Decode<'de>>() {}
+
+fn main() {
+    assert_derive::<'_, One<u8>>();
+    assert_derive::<'_, Two<u8>>();
+}
