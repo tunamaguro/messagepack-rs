bd14cf667502f9839dba6392e0468dd65302796c
tunamaguro <contact@tunamaguro.dev>
2026-04-09 11:06:32 +0000

    example

--- messagepack-derive/tests/success/basic_struct.rs
+++ messagepack-derive/tests/success/basic_struct.rs
@@ -11,7 +11,7 @@ struct Point {
 }
 
 // expect expands to below
-// 
+//
 // impl Encode for Point {
 //     fn encode<__W: io::IoWrite>(
 //         &self,
@@ -35,6 +35,144 @@ struct Point {
 //         '__msgpack_de: '__reader,
 //     {
 //         todo!("some implementation")
+//     }
+// }
+//
+// fn encode_point_as_map<__W>(
+//     point: &Point,
+//     writer: &mut __W,
+// ) -> Result<usize, messagepack_core::encode::Error<__W::Error>>
+// where
+//     __W: messagepack_core::io::IoWrite,
+// {
+//     const __FIELD_LEN: usize = 2;
+//     let mut __size = 0_usize;
+//     __size += messagepack_core::encode::Encode::encode(
+//         &messagepack_core::encode::map::MapFormatEncoder(__FIELD_LEN),
+//         writer,
+//     )?;
+//
+//     __size += messagepack_core::encode::Encode::encode(&"x", writer)?;
+//     __size += messagepack_core::encode::Encode::encode(&point.x, writer)?;
+//     __size += messagepack_core::encode::Encode::encode(&"y", writer)?;
+//     __size += messagepack_core::encode::Encode::encode(&point.y, writer)?;
+//
+//     Ok(__size)
+// }
+//
+// fn encode_point_as_array<__W>(
+//     point: &Point,
+//     writer: &mut __W,
+// ) -> Result<usize, messagepack_core::encode::Error<__W::Error>>
+// where
+//     __W: messagepack_core::io::IoWrite,
+// {
+//     const __FIELD_LEN: usize = 2;
+//     let mut __size = 0_usize;
+//     __size += messagepack_core::encode::Encode::encode(
+//         &messagepack_core::encode::array::ArrayFormatEncoder(__FIELD_LEN),
+//         writer,
+//     )?;
+//
+//     __size += messagepack_core::encode::Encode::encode(&point.x, writer)?;
+//     __size += messagepack_core::encode::Encode::encode(&point.y, writer)?;
+//
+//     Ok(__size)
+// }
+//
+// fn decode_point_map<'__msgpack_de, '__reader, __R>(
+//     len: usize,
+//     reader: &'__reader mut __R,
+// ) -> Result<Point, messagepack_core::decode::Error<__R::Error>>
+// where
+//     __R: messagepack_core::io::IoRead<'__msgpack_de>,
+// {
+//     let mut __x = None;
+//     let mut __y = None;
+//     for _ in 0..len {
+//         let key = <messagepack_core::decode::ReferenceStrBinDecoder as messagepack_core::decode::Decode<'__msgpack_de>>::decode(reader)?;
+//
+//         match key.as_bytes() {
+//             b"x" => {
+//                 if __x.is_some() {
+//                     return Err(messagepack_core::decode::Error::InvalidData);
+//                 }
+//                 __x =
+//                     Some(<u32 as messagepack_core::decode::Decode<'__msgpack_de>>::decode(reader)?);
+//             }
+//             b"y" => {
+//                 if __y.is_some() {
+//                     return Err(messagepack_core::decode::Error::InvalidData);
+//                 }
+//                 __y =
+//                     Some(<u32 as messagepack_core::decode::Decode<'__msgpack_de>>::decode(reader)?);
+//             }
+//             _ => {}
+//         }
+//     }
+//
+//     Ok(Point {
+//         x: __x.ok_or(messagepack_core::decode::Error::InvalidData)?,
+//         y: __y.ok_or(messagepack_core::decode::Error::InvalidData)?,
+//     })
+// }
+//
+// fn decode_point_array<'__msgpack_de, '__reader, __R>(
+//     len: usize,
+//     reader: &'__reader mut __R,
+// ) -> Result<Point, messagepack_core::decode::Error<__R::Error>>
+// where
+//     __R: messagepack_core::io::IoRead<'__msgpack_de>,
+// {
+//     if len != 2 {
+//         return Err(messagepack_core::decode::Error::InvalidData);
+//     }
+//     let __x = <u32 as messagepack_core::decode::Decode<'__msgpack_de>>::decode(reader)?;
+//     let __y = <u32 as messagepack_core::decode::Decode<'__msgpack_de>>::decode(reader)?;
+//     Ok(Point { x: __x, y: __y })
+// }
+//
+// fn decode_point<'__msgpack_de, '__reader, __R>(
+//     reader: &'__reader mut __R,
+// ) -> Result<Point, messagepack_core::decode::Error<__R::Error>>
+// where
+//     __R: messagepack_core::io::IoRead<'__msgpack_de>,
+// {
+//     let format =
+//         <messagepack_core::Format as messagepack_core::decode::Decode<'__msgpack_de>>::decode(
+//             reader,
+//         )?;
+//
+//     enum FormatKind {
+//         Map(usize),
+//         Array(usize),
+//     }
+//     let kind = match format {
+//         messagepack_core::Format::FixMap(n) => FormatKind::Map(n.into()),
+//         messagepack_core::Format::FixArray(n) => FormatKind::Array(n.into()),
+//
+//         messagepack_core::Format::Array16 => {
+//             let len = messagepack_core::decode::NbyteReader::<2>::read(reader)?;
+//             FormatKind::Array(len)
+//         }
+//         messagepack_core::Format::Map16 => {
+//             let len = messagepack_core::decode::NbyteReader::<2>::read(reader)?;
+//             FormatKind::Map(len)
+//         }
+//         messagepack_core::Format::Array32 => {
+//             let len = messagepack_core::decode::NbyteReader::<4>::read(reader)?;
+//             FormatKind::Array(len)
+//         }
+//         messagepack_core::Format::Map32 => {
+//             let len = messagepack_core::decode::NbyteReader::<4>::read(reader)?;
+//             FormatKind::Map(len)
+//         }
+//         _ => return Err(messagepack_core::decode::Error::UnexpectedFormat),
+//     };
+//
+//     match kind {
+//         FormatKind::Map(len) => decode_point_map(len, reader),
+//         FormatKind::Array(len) => decode_point_array(len, reader),
 //     }
 // }
 
