e91ec0e442475c48354388b31363918e2989e171
tunamaguro <contact@tunamaguro.dev>
2026-04-09 08:53:25 +0000

    add example

--- messagepack-derive/tests/success/generics.rs
+++ messagepack-derive/tests/success/generics.rs
@@ -9,6 +9,42 @@ struct Wrapper<T> {
     value: T,
 }
 
+// expect expands to below
+// 
+// impl<T> Encode for Wrapper<T>
+// where
+//     T: Encode,
+// {
+//     fn encode<__W: io::IoWrite>(
+//         &self,
+//         writer: &mut __W,
+//     ) -> Result<usize, encode::Error<__W::Error>> {
+//         todo!("some implementation")
+//     }
+// }
+
+// impl<'__msgpack_de, T> Decode<'__msgpack_de> for Wrapper<T>
+// where
+//     T: Decode<'__msgpack_de>,
+// {
+//     type Value<'__reader>
+//         = Wrapper<T::Value<'__reader>>
+//     where
+//         Self: '__reader,
+//         '__msgpack_de: '__reader;
+
+//     fn decode_with_format<'__reader, __R>(
+//         format: Format,
+//         reader: &'__reader mut __R,
+//     ) -> Result<Self::Value<'__reader>, decode::Error<__R::Error>>
+//     where
+//         __R: io::IoRead<'__msgpack_de>,
+//         '__msgpack_de: '__reader,
+//     {
+//         todo!("some implementation")
+//     }
+// }
+
 fn main() {
     let w = Wrapper { value: 42u32 };
     let mut buf = Vec::new();
