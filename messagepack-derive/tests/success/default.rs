f1b0431e291eb2399b4432192c919a5a33713fb5
tunamaguro <contact@tunamaguro.dev>
2026-04-10 14:09:54 +0000

    feat: add tests for default handling in derive_encode and derive_decode

---
+++ messagepack-derive/tests/success/default.rs
@@ -0,0 +1,22 @@
+use messagepack_derive::{Decode, Encode};
+
+use messagepack_core::{decode::Decode, encode::Encode};
+
+#[derive(Encode, Decode)]
+struct S1<T> {
+    #[msgpack(default)]
+    foo: Option<T>,
+}
+
+fn assert_derive<'de, T: Encode + Decode<'de>>() {}
+
+#[derive(Encode, Decode)]
+struct S2<T>(#[msgpack(default)] Option<T>);
+
+
+struct NoDefault;
+
+fn main() {
+    assert_derive::<S1<NoDefault>>();
+    assert_derive::<S2<NoDefault>>();
+}
