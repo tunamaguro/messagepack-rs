e788fe451d6ca954e86c701811fbf5a5d8f7ad1f
tunamaguro <contact@tunamaguro.dev>
2026-04-12 05:42:06 +0000

    wip

--- messagepack-derive/tests/fail/name_value_attr.rs
+++
@@ -1,9 +0,0 @@
-use messagepack_derive::{Decode, Encode};
-
-#[derive(Encode, Decode)]
-struct S {
-    #[msgpack = "bytes"]
-    value: u8,
-}
-
-fn main() {}
--- messagepack-derive/tests/fail/tuple_as_map.rs
+++
@@ -1,7 +0,0 @@
-use messagepack_derive::{Decode, Encode};
-
-#[derive(Encode, Decode)]
-#[msgpack(map)]
-struct TupleAsMap(String, u8);
-
-fn main() {}
